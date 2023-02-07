88903ff3940bd534b61a80f97ed4fd2b7be5d11b
Iago Viotti <iago.viotti@gmail.com>
2023-02-07 11:37:37 -0300

    req 2

--- src/index.js
+++ src/index.js
@@ -16,9 +16,25 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
-
+/** req 1 */
 app.get('/talker', async (_req, res) => {
   const contentPath = path.resolve(__dirname, 'talker.json');
   const content = await fs.readFile(contentPath, 'utf-8');
   res.status(HTTP_OK_STATUS).json(JSON.parse(content));
+  console.log(typeof content);
 });
+
+/** req 2 */
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const contentPath = path.resolve(__dirname, 'talker.json');
+  const content = await fs.readFile(contentPath, 'utf-8');
+  const jsonContent = JSON.parse(content);
+  const response = jsonContent.find((talker) => talker.id === Number(id));
+  if (!response) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).json(response);
+});
+
+/** req 3 */
